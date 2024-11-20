864e7e1fdd6c7429e39e3c16acd162be68feacbe
Rancic <me@rancic.org>
2024-11-20 11:38:44 +0100

    feat: Add terminal and tool packages

--- home.nix
+++ home.nix
@@ -1,21 +1,44 @@
 { config, pkgs, ... }: {
   home = {
     packages = with pkgs; [
-      yazi
+      signal-desktop
       pass
       brave
-      zoxide
       scc
+      libresprite
+      nasm
+      imagemagick
+      libreoffice
+      vieb
+      git-filter-repo
+      cpulimit
+      # You also want to copy the mpv files, maybe that could be a good first test with Le Nam?
       # mpv
+
+      # # Terminal stuff
+      # alacritty - not up to date in nix
+      # zellij - not up to date in nix
+      # nu - not up to date!
+      # starship - not up to date!
+      # Useful tools
+      bat
+      bottom
+      du-dust
+      eza
+      zoxide
+      yazi
+      ripgrep
+      # Game dev
+      wasm-bindgen-cli
     ];
 
-    file = {
-      ".config/my_text_file.txt".text = "Hello World, how are you!";
-      ".config/nvim/syntax/yarn.vim".text = builtins.readFile ./files/yarn.vim;
-    };
+    # file = {
+    #   ".config/my_text_file.txt".text = "Hello World, how are you!";
+    #   ".config/nvim/syntax/yarn.vim".text = builtins.readFile ./files/yarn.vim;
+    # };
 
-    username = "anto";
-    homeDirectory = "/home/anto";
+    username = "rancic";
+    homeDirectory = "/home/rancic";
     stateVersion = "24.05";
   };
 
